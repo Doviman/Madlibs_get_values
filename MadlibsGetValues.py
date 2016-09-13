3620d109db1ca8ce39494c40c5c71771aed5cea1
Doviman <disaacman@students.mchenry.edu>
2016-09-13 14:04:10 -0500

    Add files via upload

---
+++ MadlibsGetValues.py
@@ -0,0 +1,52 @@
+
+print "Enter a persons first name"
+person = raw_input()
+if person == "":
+    name = "Rambo"
+
+print "Enter a noun"
+part1 = raw_input()
+if part1 == "":
+    part1 = "fist"
+
+print "Enter a noun"
+part2 = raw_input()
+if part2 == "":
+    part2 = "missle launcher"
+
+print "Enter an action"
+action1 = raw_input()
+if action1 == "":
+    action1 = "chew"
+
+print "Enter action"
+action2 = raw_input()
+if action2 == "":
+    action2 = "spit"
+
+print "Enter adverb"
+adverb = raw_input()
+if adverb == "":
+    adverb = "fiesty"
+
+print "Enter noun"
+noun = raw_input()
+if noun == "":
+    noun = "ninja warrior"
+
+
+
+
+# Poem
+print(person + " is a " + adverb + " " + noun + ",")
+print("Short and stout,")
+print("Here is a " + person + " " + part1 + ",")
+print("Here is a " + person + " " + part2 + ",")
+print("When " + person + " get all steamed up,")
+print("Hear " + person + " shout,")
+print(action1 + " " + person + " up and " + action2 + " " + person + " out! ")
+print(person + " is a clever " + noun + ",")
+print("Yes, it's true,")
+print("Here's an example of what " + person + " can do,")
+print(person + " can turn his " + part1 + " into a " + part2 + ",")
+print(action1 + " " + person + " over and " + action2 + " " + person + " out!")
